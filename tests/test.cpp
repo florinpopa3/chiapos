florinpopa3/chiapos

commit 24e3057e05fcaeae040b115d49d5452a1cfa064e

prover_disk|tests: Make `DiskProver` serializable

src/prover_disk.hpp
@@ -33,6 +33,7 @@
 #include "calculate_bucket.hpp"
 #include "encoding.hpp"
 #include "entry_sizes.hpp"
+#include "serialize.hpp"
 #include "util.hpp"
 
 struct plot_header {
@@ -48,6 +49,7 @@ struct plot_header {
 // of space, for a given challenge.
 class DiskProver {
 public:
+    static const uint16_t VERSION{1};
     // The constructor opens the file, and reads the contents of the file header. The table pointers
     // will be used to find and seek to all seven tables, at the time of proving.
     explicit DiskProver(const std::string& filename) : id(kIdLen)
@@ -115,6 +117,22 @@ public:
         }
 
         delete[] c2_buf;
+    }
+
+    explicit DiskProver(const std::vector<uint8_t>& vecBytes)
+    {
+        Deserializer deserializer(vecBytes);
+        deserializer >> version;
+        if (version != VERSION) {
+            // TODO: Migrate to new version if we change something related to the data structure
+            throw std::invalid_argument("DiskProver: Invalid version.");
+        }
+        deserializer >> filename;
+        deserializer >> memo;
+        deserializer >> id;
+        deserializer >> k;
+        deserializer >> table_begin_pointers;
+        deserializer >> C2;
     }
 
     ~DiskProver()
@@ -233,7 +251,15 @@ public:
         return full_proof;
     }
 
+    std::vector<uint8_t> ToBytes() const
+    {
+        Serializer serializer;
+        serializer << version << filename << memo << id << k << table_begin_pointers << C2;
+        return serializer.Data();
+    }
+
 private:
+    uint16_t version{VERSION};
     mutable std::mutex _mtx;
     std::string filename;
     std::vector<uint8_t> memo;

tests/test.cpp
@@ -225,6 +225,33 @@ TEST_CASE("(De)Serialization")
         REQUIRE(str2_deserialized == str2);
         REQUIRE(deserializer.End());
     }
+    SECTION("DiskProver")
+    {
+        std::string filename = "prover_test.plot";
+        DiskPlotter plotter = DiskPlotter();
+        uint8_t memo[5] = {1, 2, 3, 4, 5};
+        plotter.CreatePlotDisk(
+            ".", ".", ".", filename, 18, memo, 5, plot_id_1, 32, 11, 0, 4000, 2);
+        DiskProver prover1(filename);
+        std::vector<uint8_t> vecBytes = prover1.ToBytes();
+        DiskProver prover2(vecBytes);
+        REQUIRE(prover1.GetFilename() == prover2.GetFilename());
+        REQUIRE(prover1.GetSize() == prover2.GetSize());
+        REQUIRE(prover1.GetId() == prover2.GetId());
+        REQUIRE(prover1.GetMemo() == prover2.GetMemo());
+        vector<unsigned char> hash_input = intToBytes(0, 4);
+        vector<unsigned char> hash(picosha2::k_digest_size);
+        picosha2::hash256(hash_input.begin(), hash_input.end(), hash.begin(), hash.end());
+        vector<LargeBits> qualities1 = prover1.GetQualitiesForChallenge(hash.data());
+        LargeBits proof1 = prover1.GetFullProof(hash.data(), 0);
+        vector<LargeBits> qualities2 = prover2.GetQualitiesForChallenge(hash.data());
+        LargeBits proof2 = prover2.GetFullProof(hash.data(), 0);
+        REQUIRE(qualities1 == qualities2);
+        REQUIRE(proof1 == proof2);
+        vecBytes[0] = 0x02; // Change version
+        REQUIRE_THROWS(DiskProver(vecBytes)); // Invalid version
+        REQUIRE(remove(filename.c_str()) == 0);
+    }
 }
 TEST_CASE("Util")
 {

commit 068708a7d208dd55505123de5a8de4bb1d289cca

src|tests|python-bindings: Make `DiskProver`(de)serializable (#323)
* serialize|tests: Implement basic serialization and corresponding tests

* prover_disk|tests: Make `DiskProver` serializable

* prover_disk|tests: Implement and test move constructor for `DiskProver`

* python-bindings|tests: Introduce `DiskProver.{__bytes__|from_bytes}`

* tests: Move `(De)serialization` test case down in hierarchy

This is to work around an issue in the `blake3` source code namely the 
following function having a "init phase" which isn't thread safe 
(Assigning the global variable `g_cpu_features` if its undefined yet):

https://github.com/BLAKE3-team/BLAKE3/blob/4056af6d7ffdf4d13bb776b7ea1db2a6b52d4d75/c/blake3_dispatch.c#L85 

Since it's not thread safe it lead to data race here prior to this 
commit because 24e3057e05fcaeae040b115d49d5452a1cfa064e introduced the 
`(De)Serialization` test as first test in the file and it triggeres plot 
creation with 2 phase1 threads which trigger `get_cpu_features` and in 
this case concurrently. The workaround here is to move the new test 
below the `F Functions` test which is the first test in the file which 
is calling `get_cpu_features` from a single thread. This single thread 
runs the "init phase" and since this is only required once the follow up 
tests are as fine like they were before on `main`.

* prover_disk: Explicitly delete the copy constructor

* prover_disk: Don't lock the mutex in move contructor

* prover_disk: Move the `version` field also

* serialize: Be more specific in some cases about the in/out type

* serialize: Constrain functions in the template to trivial types

* serialize: `elements` -> `size` and `size` -> `offset`

python-bindings/chiapos.cpp
@@ -77,6 +77,21 @@ PYBIND11_MODULE(chiapos, m)
 
     py::class_<DiskProver>(m, "DiskProver")
         .def(py::init<const std::string &>(), py::call_guard<py::gil_scoped_release>())
+        .def_static("from_bytes", [](const py::bytes &bytes) -> DiskProver {
+            py::buffer_info info(py::buffer(bytes).request());
+            auto data = reinterpret_cast<const uint8_t*>(info.ptr);
+            auto vecBytes = std::vector<uint8_t>(data, data + info.size);
+            py::gil_scoped_release release;
+            return DiskProver(vecBytes);
+        })
+        .def("__bytes__", [](DiskProver &dp) {
+            std::vector<uint8_t> vecBytes;
+            {
+                py::gil_scoped_release release;
+                vecBytes = dp.ToBytes();
+            }
+            return py::bytes(reinterpret_cast<const char*>(vecBytes.data()), vecBytes.size());
+        })
         .def(
             "get_memo",
             [](DiskProver &dp) {

src/prover_disk.hpp
@@ -33,6 +33,7 @@
 #include "calculate_bucket.hpp"
 #include "encoding.hpp"
 #include "entry_sizes.hpp"
+#include "serialize.hpp"
 #include "util.hpp"
 
 struct plot_header {
@@ -48,6 +49,7 @@ struct plot_header {
 // of space, for a given challenge.
 class DiskProver {
 public:
+    static const uint16_t VERSION{1};
     // The constructor opens the file, and reads the contents of the file header. The table pointers
     // will be used to find and seek to all seven tables, at the time of proving.
     explicit DiskProver(const std::string& filename) : id(kIdLen)
@@ -117,6 +119,35 @@ public:
         delete[] c2_buf;
     }
 
+    explicit DiskProver(const std::vector<uint8_t>& vecBytes)
+    {
+        Deserializer deserializer(vecBytes);
+        deserializer >> version;
+        if (version != VERSION) {
+            // TODO: Migrate to new version if we change something related to the data structure
+            throw std::invalid_argument("DiskProver: Invalid version.");
+        }
+        deserializer >> filename;
+        deserializer >> memo;
+        deserializer >> id;
+        deserializer >> k;
+        deserializer >> table_begin_pointers;
+        deserializer >> C2;
+    }
+
+    DiskProver(DiskProver const&) = delete;
+
+    DiskProver(DiskProver&& other) noexcept
+    {
+        filename = std::move(other.filename);
+        memo = std::move(other.memo);
+        id = std::move(other.id);
+        k = other.k;
+        table_begin_pointers = std::move(other.table_begin_pointers);
+        C2 = std::move(other.C2);
+        version = std::move(other.version);
+    }
+
     ~DiskProver()
     {
         std::lock_guard<std::mutex> l(_mtx);
@@ -130,7 +161,11 @@ public:
 
     const std::vector<uint8_t>& GetId() { return id; }
 
-    std::string GetFilename() const noexcept { return filename; }
+    const std::vector<uint64_t>& GetTableBeginPointers() const noexcept { return table_begin_pointers; }
+
+    const std::vector<uint64_t>& GetC2() const noexcept { return C2; }
+
+    const std::string& GetFilename() const noexcept { return filename; }
 
     uint8_t GetSize() const noexcept { return k; }
 
@@ -233,7 +268,15 @@ public:
         return full_proof;
     }
 
+    std::vector<uint8_t> ToBytes() const
+    {
+        Serializer serializer;
+        serializer << version << filename << memo << id << k << table_begin_pointers << C2;
+        return serializer.Data();
+    }
+
 private:
+    uint16_t version{VERSION};
     mutable std::mutex _mtx;
     std::string filename;
     std::vector<uint8_t> memo;

src/serialize.hpp
@@ -1,0 +1,153 @@
+// Copyright 2022 Chia Network Inc
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_SERIALIZE_HPP_
+#define SRC_SERIALIZE_HPP_
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+template<typename Type> class Serializable{
+public:
+    static void SerializeImpl(const Type& in, std::vector<uint8_t>& out)
+    {
+        static_assert(std::is_trivial_v<Type>);
+        size_t nSize = sizeof(in);
+        out.reserve(out.size() + nSize);
+        for (size_t i = 0; i < nSize; ++i) {
+            out.push_back(*((uint8_t*)&in + i));
+        }
+    }
+    static size_t DeserializeImpl(const std::vector<uint8_t>& in, Type& out, size_t position)
+    {
+        static_assert(std::is_trivial_v<Type>);
+        size_t size = sizeof(out);
+        if (position + size > in.size()) {
+            throw std::invalid_argument("DeserializeImpl: Trying to read out of bounds.");
+        }
+        for (size_t i = 0; i < size; ++i) {
+            *((uint8_t*)&out + i) = in[position + i];
+        }
+        return size;
+    }
+};
+
+template<typename TypeIn>
+void Serialize(const TypeIn& in, std::vector<uint8_t>& out)
+{
+    Serializable<TypeIn>::SerializeImpl(in, out);
+}
+
+template<typename TypeOut>
+size_t Deserialize(const std::vector<uint8_t>& in, TypeOut& out, const size_t position)
+{
+    return Serializable<TypeOut>::DeserializeImpl(in, out, position);
+}
+
+template<typename TypeIn>
+void SerializeContainer(const TypeIn& in, std::vector<uint8_t>& out)
+{
+    Serialize(in.size(), out);
+    for (auto& entry : in) {
+        Serialize(entry, out);
+    }
+}
+
+template<typename TypeOut>
+size_t DeserializeContainer(const std::vector<uint8_t>& in, TypeOut& out, const size_t position)
+{
+    size_t size;
+    size_t offset = Deserialize(in, size, position);
+    if (size == 0) {
+        return offset;
+    }
+    out.clear();
+    out.resize(size);
+    for (size_t i = 0; i < size; ++i) {
+        offset += Deserialize(in, out[i], position + offset);
+    }
+    return offset;
+}
+
+template<typename Type>
+class Serializable<std::vector<Type>>{
+public:
+    static void SerializeImpl(const std::vector<Type>& in, std::vector<uint8_t>& out)
+    {
+        SerializeContainer(in, out);
+    }
+    static size_t DeserializeImpl(const std::vector<uint8_t>& in, std::vector<Type>& out, const size_t position)
+    {
+        return DeserializeContainer(in, out, position);
+    }
+};
+
+template<>
+class Serializable<std::string>{
+public:
+    static void SerializeImpl(const std::string& in, std::vector<uint8_t>& out)
+    {
+        SerializeContainer(in, out);
+    }
+    static size_t DeserializeImpl(const std::vector<uint8_t>& in, std::string& out, const size_t position)
+    {
+        return DeserializeContainer(in, out, position);
+    }
+};
+
+class Serializer
+{
+    std::vector<uint8_t> data;
+public:
+    template <typename InputType>
+    friend Serializer& operator <<(Serializer& serializer, const InputType& value) {
+        Serialize(value, serializer.data);
+        return serializer;
+    }
+    std::vector<uint8_t>& Data()
+    {
+        return data;
+    }
+    void Reset()
+    {
+        data.clear();
+    }
+};
+
+
+class Deserializer
+{
+    size_t position{0};
+    const std::vector<uint8_t>& data;
+public:
+    explicit Deserializer(const std::vector<uint8_t>& data) : data(data) {}
+    void Reset()
+    {
+        position = 0;
+    }
+    template <typename OutputType>
+    friend Deserializer& operator>>(Deserializer& deserializer, OutputType& output) {
+        deserializer.position += Deserialize(deserializer.data,
+                                             output,
+                                             deserializer.position);
+        return deserializer;
+    }
+    bool End() const
+    {
+        return position == data.size();
+    }
+};
+
+#endif  // SRC_SERIALIZE_HPP_

tests/test.cpp
@@ -23,6 +23,7 @@
 #include "disk.hpp"
 #include "plotter_disk.hpp"
 #include "prover_disk.hpp"
+#include "serialize.hpp"
 #include "sort_manager.hpp"
 #include "verifier.hpp"
 
@@ -144,7 +145,6 @@ TEST_CASE("SliceInt64FromBytesFull")
     CHECK(Util::SliceInt64FromBytesFull(bytes, 7, 64) == ((0x1020304050607080ull << 3) | 0b100));
     CHECK(Util::SliceInt64FromBytesFull(bytes, 8, 64) == 0x0203040506070809ull);
 }
-
 TEST_CASE("Util")
 {
     SECTION("Increment and decrement")
@@ -477,6 +477,115 @@ TEST_CASE("F functions")
         VerifyFC(6, 16, 0xd420677f6cbd, 0x5894aa2ca1af, 0x2efde9, 0xc2121, 0x421bb8ec);
         VerifyFC(7, 16, 0x5fec898f, 0x82283d15, 0x14f410, 0x24c3c2, 0x0);
         VerifyFC(7, 16, 0x64ac5db9, 0x7923986, 0x590fd, 0x1c74a2, 0x0);
+    }
+}
+
+TEST_CASE("(De)Serialization")
+{
+    Serializer serializer;
+    Deserializer deserializer(serializer.Data());
+    SECTION("Basics")
+    {
+        serializer.Reset();
+        for (uint8_t i = 0; i < 0xFF; ++i) {
+            serializer << i;
+        }
+        uint8_t n{0}, n_deserialized;
+        do{
+            deserializer >> n_deserialized;
+            REQUIRE(n_deserialized == n++);
+        } while (!deserializer.End());
+        serializer.Reset();
+        deserializer.Reset();
+        std::vector<float> vecFloat;
+        REQUIRE_THROWS(deserializer >> vecFloat); // Unexpected vector, leads to read out of bounds
+        float f = 0.123, f_deserialized = 0;
+        double d = 1.23, d_deserialized = 0;
+        int64_t i = -123, i_deserialized = 0;
+        uint64_t u = 123, u_deserialized = 0;
+        serializer << f;
+        deserializer >> f_deserialized;
+        REQUIRE(f_deserialized == f);
+        serializer << d;
+        deserializer >> d_deserialized;
+        REQUIRE(d_deserialized == d);
+        serializer << i;
+        deserializer >> i_deserialized;
+        REQUIRE(i_deserialized == i);
+        serializer << u;
+        deserializer >> u_deserialized;
+        REQUIRE(u_deserialized == u);
+        REQUIRE(deserializer.End());
+        deserializer.Reset();
+        deserializer >> f_deserialized;
+        deserializer >> d_deserialized;
+        deserializer >> i_deserialized;
+        deserializer >> u_deserialized;
+        REQUIRE(f_deserialized == f);
+        REQUIRE(d_deserialized == d);
+        REQUIRE(i_deserialized == i);
+        REQUIRE(u_deserialized == u);
+        REQUIRE(deserializer.End());
+        REQUIRE_THROWS(deserializer >> f_deserialized); // Read out of bounds
+    }
+    SECTION("vector")
+    {
+        std::vector<uint64_t> vec1{1,2,3}, vec2;
+        std::vector<std::vector<uint64_t>> vec3{{1}, {2,3}, {4,5,6}}, vec4;
+        serializer << vec1;
+        serializer << vec2;
+        serializer << vec3;
+        serializer << vec4;
+        std::vector<uint64_t> vec1_deserialized, vec2_deserialized;
+        std::vector<std::vector<uint64_t>> vec3_deserialized, vec4_deserialized;
+        deserializer >> vec1_deserialized;
+        deserializer >> vec2_deserialized;
+        deserializer >> vec3_deserialized;
+        deserializer >> vec4_deserialized;
+        REQUIRE(vec1_deserialized == vec1);
+        REQUIRE(vec2_deserialized == vec2);
+        REQUIRE(vec3_deserialized == vec3);
+        REQUIRE(vec4_deserialized == vec4);
+        REQUIRE(deserializer.End());
+    }
+    SECTION("string")
+    {
+        std::string str1 = "123", str2;
+        serializer << str1;
+        serializer << str2;
+        std::string str1_deserialized, str2_deserialized;
+        deserializer >> str1_deserialized;
+        deserializer >> str2_deserialized;
+        REQUIRE(str1_deserialized == str1);
+        REQUIRE(str2_deserialized == str2);
+        REQUIRE(deserializer.End());
+    }
+    SECTION("DiskProver")
+    {
+        std::string filename = "prover_test.plot";
+        DiskPlotter plotter = DiskPlotter();
+        uint8_t memo[5] = {1, 2, 3, 4, 5};
+        plotter.CreatePlotDisk(
+            ".", ".", ".", filename, 18, memo, 5, plot_id_1, 32, 11, 0, 4000, 2);
+        DiskProver prover1(filename);
+        std::vector<uint8_t> vecBytes = prover1.ToBytes();
+        DiskProver prover2(vecBytes);
+        REQUIRE(prover1.GetFilename() == prover2.GetFilename());
+        REQUIRE(prover1.GetSize() == prover2.GetSize());
+        REQUIRE(prover1.GetId() == prover2.GetId());
+        REQUIRE(prover1.GetMemo() == prover2.GetMemo());
+        vector<unsigned char> hash_input = intToBytes(0, 4);
+        vector<unsigned char> hash(picosha2::k_digest_size);
+        picosha2::hash256(hash_input.begin(), hash_input.end(), hash.begin(), hash.end());
+        vector<LargeBits> qualities1 = prover1.GetQualitiesForChallenge(hash.data());
+        LargeBits proof1 = prover1.GetFullProof(hash.data(), 0);
+        vector<LargeBits> qualities2 = prover2.GetQualitiesForChallenge(hash.data());
+        LargeBits proof2 = prover2.GetFullProof(hash.data(), 0);
+        REQUIRE(qualities1 == qualities2);
+        REQUIRE(proof1 == proof2);
+        vecBytes[0] = 0x02; // Change version
+        REQUIRE_THROWS(DiskProver(vecBytes)); // Invalid version
+        REQUIRE(remove(filename.c_str()) == 0);
     }
 }
 
@@ -965,6 +1074,38 @@ TEST_CASE("BufferedDisk")
     }
 
     remove("test_file.bin");
+}
+
+TEST_CASE("DiskProver")
+{
+    SECTION("Move constructor")
+    {
+        std::string filename = "prover_test.plot";
+        DiskPlotter plotter = DiskPlotter();
+        std::vector<uint8_t> memo{1, 2, 3};
+        plotter.CreatePlotDisk(
+            ".", ".", ".", filename, 18, memo.data(),
+            memo.size(), plot_id_1, 32, 11, 0,
+            4000, 2);
+        DiskProver prover1(filename);
+        auto* p1_filename_ptr = prover1.GetFilename().data();
+        auto* p1_memo_ptr = prover1.GetMemo().data();
+        auto* p1_id_ptr = prover1.GetId().data();
+        auto* p1_table_begin_pointers_ptr = prover1.GetTableBeginPointers().data();
+        auto* p1_C2_ptr = prover1.GetC2().data();
+        DiskProver prover2(std::move(prover1));
+        REQUIRE(prover2.GetFilename().data() == p1_filename_ptr);
+        REQUIRE(prover2.GetMemo().data() == p1_memo_ptr);
+        REQUIRE(prover2.GetId().data() == p1_id_ptr);
+        REQUIRE(prover2.GetTableBeginPointers().data() == p1_table_begin_pointers_ptr);
+        REQUIRE(prover2.GetC2().data() == p1_C2_ptr);
+        REQUIRE(prover1.GetFilename().empty());
+        REQUIRE(prover1.GetMemo().empty());
+        REQUIRE(prover1.GetId().empty());
+        REQUIRE(prover1.GetSize() == prover1.GetSize());
+        REQUIRE(prover1.GetTableBeginPointers().empty());
+        REQUIRE(prover1.GetC2().empty());
+    }
 }
 
 TEST_CASE("FilteredDisk")

tests/test_python_bindings.py
@@ -176,6 +176,30 @@ class TestPythonBindings(unittest.TestCase):
         print(f"Successes: {successes}")
         print(f"Failures: {failures}")
 
+    def test_disk_prover_byte_conversions(self):
+        plot_path = Path("byte_conversion_plot.dat")
+        if plot_path.exists():
+            plot_path.unlink()
+        challenge = sha256(b"\x00").digest()
+        pl = DiskPlotter()
+        pl.create_plot_disk(
+            ".", ".", ".", str(plot_path), 21, bytes([1, 2, 3, 4, 5]), bytes(b'\0' * 32), 300, 32, 8192, 8, False
+        )
+        pr = DiskProver(str(plot_path))
+        serialized = bytes(pr)
+        pr_recovered = DiskProver.from_bytes(serialized)
+        assert bytes(pr_recovered) == serialized
+        assert pr.get_memo() == pr_recovered.get_memo()
+        assert pr.get_id() == pr_recovered.get_id()
+        assert pr.get_size() == pr_recovered.get_size()
+        assert pr.get_filename() == pr_recovered.get_filename()
+        assert pr.get_size() == pr_recovered.get_size()
+        assert pr.get_qualities_for_challenge(challenge) == pr_recovered.get_qualities_for_challenge(challenge)
+        with self.assertRaises(ValueError):
+            DiskProver.from_bytes(bytes())
+        with self.assertRaises(ValueError):
+            DiskProver.from_bytes(serialized[0:int(len(serialized)/2)])
+
 
 if __name__ == "__main__":
     unittest.main()